90107bfb20386f4bd8af1b668d5b01e411023335
Parag07c <choudharyparag001@gmail.com>
2022-11-29 14:17:42 +0530

    created schema

---
+++ src/models/bookModel.js
@@ -0,0 +1,52 @@
+const mongoose =require("mongoose")
+
+const ObjectId=mongoose.Schema.Types.ObjectId
+
+const bookSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    excerpt:{
+        type:String,
+        required:true
+    },
+    userId:{
+         type:ObjectId,
+         required:true,
+         ref:'users'
+    },
+    ISBN:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    subcategory:{
+        type:String,
+        required:true
+    },
+    reviews:{
+        type:Number,
+        default:0
+    },
+    deletedAt:{
+        type:Date
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    releasedAt:{
+        type:Date,
+        required:true
+    }
+
+},{timestamps:true})
+
+module.exports=mongoose.model("books",bookSchema)
+
