998ee3e4b541c0a42791192f357d0e2bb0e3605e
Parag07c <choudharyparag001@gmail.com>
2022-11-30 16:06:17 +0530

    modify validator

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,7 @@
 const userModel = require("../models/userModel.js")
 const jwt = require("jsonwebtoken")
+const {isValid,isValidName,isValidMobile,validateEmail,isValidPassword}=require("../validator/validation.js")
 
-function isValid(value) {
-    if (typeof value === 'string' && value.trim().length == 0) return false
-    if (value === 'undefined' || value === null) return false
-    return true
-
-}
-const isValidName = function (value) {
-    name = value.trim()
-    return (/^([a-zA-Z'-.]+(?: [a-zA-Z'-.]+)?)$/.test(name))
-}
-const isValidMobile = function (phone) {
-    phone = phone.trim()
-    return (/^[0-9]\d{9}$/.test(phone))
-}
-const validateEmail = function (email) {
-    email = email.trim()
-    return (/^[a-z0-9_]{3,}@[a-z]{3,}[.]{1}[a-z]{3,6}$/).test(email)
-}
-const isValidPassword = function (password) {
-    password = password.trim()
-    return (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{8,15}$/).test(password)
-}
 const createUser = async function (req, res) {
     try {
         let data = req.body
