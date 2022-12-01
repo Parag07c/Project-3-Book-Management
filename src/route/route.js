154a8c8203a6f5a59b48f120057a5c5c7b9aa025
Parag07c <choudharyparag001@gmail.com>
2022-12-01 10:39:58 +0530

    route add

--- src/route/route.js
+++ src/route/route.js
@@ -9,14 +9,11 @@ router.post('/login',userController.userLogin)
 router.post("/books",bookController.createBook)
 router.get("/books",bookController.getBooks)
 router.get("/books/:bookId",bookController.getBookById)
-
-
-
-
-
-
-
-
+router.put("/books/:bookId",bookController.updateBook)
+router.delete("/books/:bookId",bookController.deleteBook)
+router.post("/books/:bookId/review",reviewController.createReviews)
+router.put("/books/:bookId/review/:reviewId",reviewController.updateReviews)
+router.delete("/books/:bookId/review/:reviewId",reviewController.deleteReviews)
 
 
 module.exports = router
